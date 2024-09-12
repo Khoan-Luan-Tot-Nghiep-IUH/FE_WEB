49a03adcc5c6340e9641cbd4433b630a71b32038
TranTheThanh2k2 <tranthethanh852002@gmail.com>
2024-09-12 16:20:37 +0700

    fix notification

--- src/components/routes/PrivateRoutes.js
+++ src/components/routes/PrivateRoutes.js
@@ -5,16 +5,13 @@ const PrivateRoute = ({ children, userInfo, requiredRole }) => {
   const location = useLocation();
 
   if (!userInfo) {
-    // Nếu chưa đăng nhập, điều hướng tới trang đăng nhập
+
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   if (requiredRole && userInfo.roleId !== requiredRole) {
-    // Nếu không có quyền cần thiết, điều hướng tới trang không có quyền truy cập
     return <Navigate to="/not-authorized" replace />;
   }
-
-  // Hiển thị nội dung nếu người dùng có quyền truy cập
   return children;
 };
 
--- src/components/shared/SideBar/SideBar.js
+++ src/components/shared/SideBar/SideBar.js
@@ -1,13 +1,21 @@
+
 import React from 'react';
 import './Sidebar.css';
+import { FaBuilding, FaUserShield, FaUsers } from 'react-icons/fa';
 
 const Sidebar = ({ setActiveContent }) => {
   return (
     <div className="sidebar">
       <ul>
-        <li onClick={() => setActiveContent('companies')}>Quản lý Nhà Xe</li>
-        <li onClick={() => setActiveContent('admins')}>Quản lý Admin Nhà Xe</li>
-        <li onClick={() => setActiveContent('users')}>Quản lý Người Dùng</li>
+        <li onClick={() => setActiveContent('companies')} className="sidebar-item">
+          <FaBuilding /> Quản lý Nhà Xe
+        </li>
+        <li onClick={() => setActiveContent('admins')} className="sidebar-item">
+          <FaUserShield /> Quản lý Admin Nhà Xe
+        </li>
+        <li onClick={() => setActiveContent('users')} className="sidebar-item">
+          <FaUsers /> Quản lý Người Dùng
+        </li>
       </ul>
     </div>
   );
